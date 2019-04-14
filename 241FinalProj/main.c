56a2f94a20414816b9eca50a65f4b6738658a2dd
Brian Bleck <bbleck@unm.edu>
2019-04-13 23:06:20 -0600

    Initial Commit

---
+++ 241FinalProj/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  241FinalProj
+//
+//  Created by Brian on 4/13/19.
+//  Copyright © 2019 Brian Bleck. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
