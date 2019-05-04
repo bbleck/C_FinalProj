a7763cad3e481d626d2582c46cc184df50a23cde
Brian Bleck <bbleck@unm.edu>
2019-05-04 17:05:49 -0600

    Added View Class Assignments Average Grade functionality.

--- 241FinalProj/main.c
+++ 241FinalProj/main.c
@@ -1321,7 +1321,50 @@ void toViewStudentAverage(void){
  displays the averages grades for each assignment.
  **/
 void toViewClassAssignmentAvgGrades(void){
-  //todo: implement this functionality
+  int course_id = 0;
+  int assignment_id = 0;
+  int max_assignm_pts = 0;
+  int count_grades = 0;
+  int sum_grades = 0;
+  Assignment_Node *tempAssign = assignmentSentinel->next;
+  Grade_Node *tempGrade = gradeSentinel->next;
+  getCourseInt(VIEW_CLASS_ASSIGNMENT_AVERAGE_PROMPTS[0], &course_id);
+  if(!isValidCourseID(course_id)){
+    printf("Invalid course ID\n");
+    return;
+  }
+  if(tempAssign == NULL){
+    printf("No assignments in database\n");
+    return;
+  }
+  if(tempGrade == NULL){
+    printf("No grades in database\n");
+    return;
+  }
+  while(tempAssign!=assignmentSentinel){
+    max_assignm_pts = tempAssign->assignment->pts_total;
+    while(tempGrade!=gradeSentinel){
+      if(tempGrade->grade->assignment_id == assignment_id){
+        count_grades++;
+        sum_grades+=tempGrade->grade->pts_received;
+      }
+      tempGrade = tempGrade->next;
+    }
+    if(count_grades <= 0 || max_assignm_pts <= 0){
+      printf("Assignment ID #%d Error - No grades for assignment"
+             ,tempAssign->assignment->assignment_id);
+      count_grades = 0;
+      sum_grades = 0;
+      tempAssign = tempAssign->next;
+      continue;
+    }
+    printf("Assignment ID #%d Class Average: %d"
+           , tempAssign->assignment->assignment_id
+           , (sum_grades)/(max_assignm_pts*count_grades));
+    count_grades = 0;
+    sum_grades = 0;
+    tempAssign = tempAssign->next;
+  }
 }
 
 /**
